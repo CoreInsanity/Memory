1c885b0eb7623cb0580b3812fc5cefb7640a6b98
Maxim Van den Eede <maximvde@gmail.com>
2020-02-19 14:28:34 +0100

    Profile creator working again

--- src/view/PlayerCreationScreen/PlayerCreationScreenView.java
+++ src/view/PlayerCreationScreen/PlayerCreationScreenView.java
@@ -13,12 +13,12 @@ import javafx.stage.Stage;
 import models.Game;
 import scenes.Memory;
 
-public class PlayerCreationScreenView extends BorderPane {
+public class PlayerCreationScreenView extends GridPane {
     private Label lblName;
     private TextField txtName;
     private Label lblAge;
     private TextField txtAge;
-    private GridPane grid;
+    //private GridPane grid;
     private Button playBtn;
     private HBox playBtnBox;
 
@@ -32,24 +32,23 @@ public class PlayerCreationScreenView extends BorderPane {
         lblAge = new Label("Age:");
         txtAge = new TextField();
         playBtn = new Button("Play");
-        grid = new GridPane();
         playBtnBox = new HBox(10);
     }
     private void layoutNodes(){
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-        grid.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        setAlignment(Pos.CENTER);
+        setHgap(10);
+        setVgap(10);
+        setPadding(new Insets(25, 25, 25, 25));
+        setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
 
         playBtnBox.setAlignment(Pos.BOTTOM_RIGHT);
         playBtnBox.getChildren().add(playBtn);
 
-        grid.add(lblName,0, 1);
-        grid.add(txtName,1, 1);
-        grid.add(lblAge, 0, 2);
-        grid.add(txtAge, 1, 2);
-        grid.add(playBtnBox, 1, 3);
+        add(lblName,0, 1);
+        add(txtName,1, 1);
+        add(lblAge, 0, 2);
+        add(txtAge, 1, 2);
+        add(playBtnBox, 1, 3);
     }
 
     TextField getName(){return txtName;}
