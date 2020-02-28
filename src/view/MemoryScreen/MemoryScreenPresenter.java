064be462213fe11c1e9704e89ea1b4e020583df0
LarsdeLoenen <lars.deloenen@student.kdg.be>
2020-02-29 00:20:57 +0100

    Keyevents added

--- src/view/MemoryScreen/MemoryScreenPresenter.java
+++ src/view/MemoryScreen/MemoryScreenPresenter.java
@@ -6,10 +6,10 @@ import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import models.Tile;
 import view.MainMenuScreen.MainMenuScreenPresenter;
@@ -19,12 +19,14 @@ import view.PlayerCreationScreen.PlayerCreationScreenView;
 import view.ScoreboardScreen.ScoreboardScreenPresenter;
 import view.ScoreboardScreen.ScoreboardScreenView;
 
+import javax.swing.border.StrokeBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.EventListener;
+
 
 /**
  * Lars De Loenen
@@ -37,21 +39,24 @@ public class MemoryScreenPresenter {
     private Image topImg;
     private Pane playField;
     private ArrayList<Image> botImgs;
+    private ArrayList<ImageView> checkmark;
     private Integer lastClickIndex;
     private Integer toRemoveIndex;
-    //private Integer toRemoveSecondIndex;
+    private Integer toRemoveSecondIndex;
+    private Image checkmarkimg;
 
     public MemoryScreenPresenter(MemoryScreenView memoryScreenView, Stage curStage) {
         stage = curStage;
         stage.setHeight(700);
         stage.setWidth(675);
         botImgs = new ArrayList<>();
+        checkmark = new ArrayList<>();
         view = memoryScreenView;
         playField = view.getPlayField();
 
         try {
             loadImgs();
-        } catch (Exception ex){
+        } catch (Exception ex) {
             // ¯\_(ツ)_/¯
             System.out.println(ex.getMessage());
             Platform.exit();
@@ -59,21 +64,28 @@ public class MemoryScreenPresenter {
 
         setCursors();
         addEventHandlers();
+        keycontrols();
     }
-    private void loadImgs() throws FileNotFoundException{
+
+    private void loadImgs() throws FileNotFoundException {
         topImg = new Image(new FileInputStream("resources\\top.png"));
-        for (var file:new File("resources\\bottom").listFiles()) {
+        checkmarkimg = new Image(new FileInputStream("resources\\checkmark.png"));
+        var checkmarkview = new ImageView(checkmarkimg);
+        checkmarkview.setFitHeight(128);
+        checkmarkview.setFitWidth(128);
+        for (var file : new File("resources\\bottom").listFiles()) {
             var img = new Image(new FileInputStream(file.getAbsolutePath()));
             botImgs.add(img);
             botImgs.add(img);
         }
         Collections.shuffle(botImgs);
     }
-    private void onTileClick(Image originalImg, ImageView view, Integer imgIndex){
+
+    private void onTileClick(Image originalImg, ImageView view, Integer imgIndex) {
         view.setImage(originalImg);
         view.setCursor(Cursor.DEFAULT);
 
-        if (toRemoveIndex != null && lastClickIndex != null){
+        if (toRemoveIndex != null && lastClickIndex != null) {
             var toRemoveImg = (ImageView) playField.getChildren().get(toRemoveIndex);
             var toRemoveSecImg = (ImageView) playField.getChildren().get(lastClickIndex);
             toRemoveImg.setImage(topImg);
@@ -82,9 +94,9 @@ public class MemoryScreenPresenter {
             lastClickIndex = null;
         }
 
-        if(lastClickIndex != null){
+        if (lastClickIndex != null) {
             var lastClickedImg = (ImageView) playField.getChildren().get(lastClickIndex);
-            if(view.getImage().hashCode() != lastClickedImg.getImage().hashCode())
+            if (view.getImage().hashCode() != lastClickedImg.getImage().hashCode())
                 toRemoveIndex = imgIndex;
             else
                 lastClickIndex = null;
@@ -93,6 +105,7 @@ public class MemoryScreenPresenter {
 
         lastClickIndex = imgIndex;
     }
+
     private void addEventHandlers() {
         view.getMenu1().setOnAction(b -> {
             var mmView = new MainMenuScreenView();
@@ -107,17 +120,57 @@ public class MemoryScreenPresenter {
         view.getMenu3().setOnAction(b -> Platform.exit());
 
         int i = 0;
-        for (var observable:playField.getChildren()) {
+        for (var observable : playField.getChildren()) {
             var index = i;
             var img = (ImageView) observable;
             img.setOnMouseClicked(m -> onTileClick(botImgs.get(index), img, index));
             i++;
         }
     }
-    private void setCursors(){
-        for (var observable:playField.getChildren()) {
+
+    private void keycontrols() {
+        view.getCheckmarkview().get(0).setVisible(true);
+        view.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            int index = 0;
+            @Override
+            public void handle(KeyEvent a) {
+                switch(a.getCode()){
+                    case A: if(index!=20){
+                        System.out.println(a.getCode());
+                        view.getCheckmarkview().get(index).setVisible(false);
+                        index++;
+                        view.getCheckmarkview().get(index).setVisible(true);
+                    } break;
+                    case LEFT: if (index>0){view.getCheckmarkview().get(index).setVisible(false); index--; view.getCheckmarkview().get(index).setVisible(true); } break;
+                    case DOWN: if (index<16){ index = index +4; } break;
+                    case UP: if (index>3){ index = index -4; } break;
+                }
+                view.getCheckmarkview().get(index).setVisible(true);
+            }
+        });
+
+
+        int a = 0;
+        for (var observable : playField.getChildren()) {
+            var index = a;
+            var img = (ImageView) observable;
+            img.setOnKeyTyped(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent e) {
+                    if (e.getCode() == KeyCode.A) {
+                        onTileClick(botImgs.get(index), img, index);
+                    }
+                }
+            });
+            a++;
+        }
+    }
+
+    private void setCursors() {
+        for (var observable : playField.getChildren()) {
             var img = (ImageView) observable;
             img.setCursor(Cursor.HAND);
         }
     }
+
 }
--- src/view/MemoryScreen/MemoryScreenView.java
+++ src/view/MemoryScreen/MemoryScreenView.java
@@ -1,10 +1,7 @@
 package view.MemoryScreen;
 
 import javafx.application.Platform;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.geometry.VPos;
+import javafx.geometry.*;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -14,6 +11,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
 import models.Game;
@@ -27,7 +25,7 @@ import java.util.List;
 
 public class MemoryScreenView extends BorderPane {
     private Image topImg;
-    private Image image;
+    private Image checkmark;
     private Pane playField;
     private MenuBar menuBar;
     private MenuItem menu1;
@@ -37,6 +35,7 @@ public class MemoryScreenView extends BorderPane {
     private Label timer;
     private Label score;
     private ImageView menulogo;
+    private List<ImageView> checkmarkview = new ArrayList<>();
 
 
     public MemoryScreenView() {
@@ -69,33 +68,50 @@ public class MemoryScreenView extends BorderPane {
         //Define the playField pane
         playField = new Pane();
     }
-    private void loadImgs() throws FileNotFoundException{
+
+    private void loadImgs() throws FileNotFoundException {
         topImg = new Image(new FileInputStream("resources\\top.png"));
+        checkmark = new Image(new FileInputStream("resources\\checkmark.png"));
         menulogo = new ImageView(new Image(new FileInputStream("resources\\tarkov.png")));
+
     }
+
     private void layoutNodes() {
         timer.setTextFill(Color.YELLOW);
         timer.setPrefWidth(50);
         timer.setMaxHeight(20);
+        timer.setAlignment(Pos.CENTER_RIGHT);
 
         score.setTextFill(Color.YELLOW);
         score.setPrefWidth(50);
         score.setMaxHeight(20);
+        score.setAlignment(Pos.CENTER_LEFT);
 
         menulogo.setFitWidth(25);
         menulogo.setFitHeight(25);
 
-        for (int i = 0; i < new File("resources\\bottom").listFiles().length*2; i++) {
+        for (int i = 0; i <20; i++) {
             var imgView = new ImageView(topImg);
             imgView.setCursor(Cursor.HAND);
             imgView.setTranslateX(128 * (i % 4));
-            imgView.setTranslateY(128 * (i / 4));
+            imgView.setTranslateY(128 * (int) (i / 4));
             playField.getChildren().add(imgView);
         }
+        for (int i = 0; i < 20; i++) {
+            var check = new ImageView(checkmark);
+            check.setVisible(false);
+            checkmarkview.add(check);
+            check.setFitWidth(64);
+            check.setFitHeight(64);
+            check.setTranslateX(128 * (i % 4));
+            check.setTranslateY(128 * (int) (i / 4));
+            playField.getChildren().add(check);
+        }
+
 
         setBackground(new Background(new BackgroundFill(Color.rgb(42, 45, 54), CornerRadii.EMPTY, Insets.EMPTY)));
         setMargin(playField, new Insets(10));
-        positionInArea(playField,10,10,10,10,10, HPos.CENTER, VPos.CENTER);
+        positionInArea(playField, 10, 10, 10, 10, 10, HPos.CENTER, VPos.CENTER);
         setPrefSize(1000, 1000);
 
         setTop(menuBar);
@@ -104,8 +120,23 @@ public class MemoryScreenView extends BorderPane {
         setRight(score);
     }
 
-    public MenuItem getMenu1() {return menu1;}
-    public MenuItem getMenu2() {return menu2;}
-    public MenuItem getMenu3() {return menu3;}
-    public Pane getPlayField() {return (Pane) getCenter();}
+    public MenuItem getMenu1() {
+        return menu1;
+    }
+
+    public MenuItem getMenu2() {
+        return menu2;
+    }
+
+    public MenuItem getMenu3() {
+        return menu3;
+    }
+
+    public Pane getPlayField() {
+        return (Pane) getCenter();
+    }
+
+    public List<ImageView> getCheckmarkview() {
+        return checkmarkview;
+    }
 }
